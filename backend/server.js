095776123f154c4d75c73ebe6f24918087144ea7
Wakeel Ogunsanya <wakeel.oguns@gmail.com>
2022-05-24 21:53:13 +0100

    Server static files from front end build folder

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,31 @@
 
 const PORT = process.env.PORT || 4000;
 
-const express = require('express')
+const fs = require('fs');
+const path = require('path');
+const express = require('express');
 const app = express()
 
+const BUILD_PATH = path.resolve('../frontend/build');
+
+//serve static files from front end build folder
+app.use(express.static(BUILD_PATH));
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+
+    filePath = BUILD_PATH + '/index.html'
+
+    if (fs.existsSync(filePath))
+    {
+        res.sendFile(filePath);
+    }
+    else
+    {
+       res.statusCode = 404;
+       res.write('404 sorry not found');
+       res.end();
+    }
+  
 })
 
 app.listen(PORT, () => {
